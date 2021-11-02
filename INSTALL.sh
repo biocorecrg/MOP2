26a254f71f79cafa9032023c492b017f9ad42102
Luca Cozzuto <lucacozzuto@gmail.com>
2021-11-02 13:54:20 +0100

    fixing missing link

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,4 +5,8 @@ mv ont-guppy mop_preprocess/bin/ont-guppy_3.4.5_linux64
 cd mop_preprocess/bin
 ln -s ont-guppy_3.4.5_linux64/ont-guppy/bin/guppy_* .
 cd ../../
+rm mop_preprocess/bin/ont-guppy_3.4.5_linux64/ont-guppy/lib/libz.so
+cd mop_preprocess/bin/ont-guppy_3.4.5_linux64/ont-guppy/lib/
+ln -s libz.so.1 libz.so
+cd ../../../
 rm -fr ont-guppy_3.4.5_linux64.tar.gz
