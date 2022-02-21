74ca8b4841d7be66a4ccc466af881490544f1449
Darek Kedra <darek.kedra@gmail.com>
2022-02-21 08:49:55 +0100

    mean_per_pos parquet data sets

--- mop_mod/bin/mean_per_pos_v3.py
+++ mop_mod/bin/mean_per_pos_v3.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
 """
-speeding up the 
-/usr/bin/time ./mean_per_pos_v3.py -i mod_batch_0.fast5_event_align.tsv.gz -o mod_batch_0.fast5_event_align.polars --mean
-Analysing data - position level - mean
-Saving results to: mod_batch_0.fast5_event_align.polars_processed_perpos_mean.parquet
-1.41user 1.14system 0:00.69elapsed 366%CPU (0avgtext+0avgdata 905180maxresident)k
-0inputs+88outputs (6major+14731minor)pagefaults 0swaps
+
 
 """
 
@@ -55,18 +50,27 @@ def mean_perpos (raw_data_polars, output_prefix):
      )
     
     result_df = q.collect()
-    result_df["read_name"] = pd.Series([1 for x in range(result_df.height)])
-    result_df = result_df["contig", "position", "reference_kmer","read_name","mean", "coverage"]
+    #result_df["read_name"] = pd.Series([1 for x in range(result_df.height)])
+    result_df = result_df["contig", "position", "reference_kmer","mean", "coverage"]
     result_df.to_parquet(output_fn)
 
     #Output parquet file:
     print(f"Saving results to: {output_fn}")
 
     
-def median_perpos (raw_data_polars, output_prefix):
+def median_perpos (raw_data_polars, output_prefix, contigs_groups):
+    """
+    default function for the script
     
-    output_fn =  f"{output_prefix}_processed_perpos_median.parquet"
+    Parameters:
+        raw_data_polars: polars dataframe 
+        output_prefix: prefix for the parquet data set dir
+        contigs_groups: dictionary contig_name: groupname 
 
+    
+    """
+    output_dir =  f"{output_prefix}.perpos_median_dset"
+    
     #Calculate mean per positions:
     print('Analysing data - position level - median')
     #sliced_data['read_name'] = 1
@@ -74,18 +78,17 @@ def median_perpos (raw_data_polars, output_prefix):
      .groupby( ["contig", "position", "reference_kmer"])
      .agg([
          (pl.col("event_level_mean").median().alias("median")), 
-         (pl.col("read_name").n_unique().alias("coverage") )])
+         (pl.col("read_name").n_unique().alias("coverage") ),
+         ])
      .sort(["contig", "position","reference_kmer" ])
     )
     
     result_df = q.collect()
-    result_df["read_name"] = pd.Series([1 for x in range(result_df.height)])
-    result_df = result_df["contig", "position", "reference_kmer","read_name","median", "coverage"]
-    result_df.to_parquet(output_fn)
-    #q.collect().to_parquet(output_fn, compression='zstd')
-    
-    #Output .csv files:
-    print(f"Saving median_perpos results to: {output_fn}")
+    #groups_series = pd.Series(contigs_groups[result_df["contig"]])
+    result_df["contig_group"] = result_df["contig"].apply(lambda x: contigs_groups.get(x))
+    result_df_arrow = result_df["contig", "contig_group", "position", "reference_kmer","median", "coverage"].to_arrow()
+    del(result_df)
+    pq.write_to_dataset(result_df_arrow, output_dir, partition_cols=["contig_group"])
     
 
 def mean_perpos_perread (raw_data, output):
@@ -101,7 +104,7 @@ def mean_perpos_perread (raw_data, output):
     pq.write_table(pa.Table.from_pandas(mean_perpos_perread), '{}_processed_perpos_perread_mean.parquete'.format(output))
 
 
-def median_perpos_perread (raw_data, output):
+def median_perpos_perread (raw_data, output, contigs_groups):
 
     #Calculate mean per positions:
     print('Analysing data - read level - median')
@@ -113,17 +116,33 @@ def median_perpos_perread (raw_data, output):
     print('Saving results to: {}_processed_perpos_perread_median.parquete'.format(output))
     pq.write_table(pa.Table.from_pandas(median_perpos_perread), '{}_processed_perpos_perread_median.parquete'.format(output))
 
+def get_contig_grps(groups_fn):
+    
+    contigs_groups_dict = {}
+    with open(groups_fn) as f:
+        for line in f:
+            line = line.strip()
+            contig, group = line.split()
+            contigs_groups_dict[contig] = group
+    return contigs_groups_dict
+
+
 def main():
     parser  = argparse.ArgumentParser(description=desc)
 
     parser.add_argument('-i', '--input', help='Input file to process.')
     parser.add_argument('-o', '--output', help='Output filename')
-    
+    parser.add_argument("-g", '--groups', help='contig group tsv')
+
     #parser.add_argument("-s", "--chunk_size", default=100000, type=int, help='Size for input subsetting [%(default)s]')
     parser.add_argument("--read_level", action='store_true', help='Analysis at per read level')
     parser.add_argument("--mean", action='store_true', help='Analysis using the mean instead of the median.')
 
     a = parser.parse_args()
+    
+    # get contigs_groups
+    contigs_groups = get_contig_grps(a.groups)
+    print(contigs_groups)
 
     #Process input:
     pd.set_option('display.precision', 2)
@@ -139,9 +158,9 @@ def main():
 
     else:
         if a.mean:
-            mean_perpos(raw_import, a.output)
+            mean_perpos(raw_import, a.output, contigs_groups)
         else:
-            median_perpos(raw_import, a.output)    
+            median_perpos(raw_import, a.output, contigs_groups)    
 
 
 if __name__=='__main__': 
