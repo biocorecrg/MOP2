f84018a9fe7f4f53977324ec92fb004fbef705a5
Luca Cozzuto <lucacozzuto@gmail.com>
2022-07-05 11:49:53 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -22,13 +22,13 @@ fi
 tar -zvxf ont-guppy_${GUPPY_VER}_linux64.tar.gz
 mv ont-guppy mop_preprocess/bin/
 cd mop_preprocess/bin
-ln -s ont-guppy/bin/guppy_* .
-ln -s ont-guppy/lib/* .
+ln -sf ont-guppy/bin/guppy_* .
+ln -sf ont-guppy/lib/* .
 cd ../../
 if [ ! -e "mop_preprocess/bin/ont-guppy/lib/libz.so" ] ; then
         unlink mop_preprocess/bin/ont-guppy/lib/libz.so
         cd mop_preprocess/bin/ont-guppy/lib/
-        ln -s libz.so.1 libz.so
+        ln -sf libz.so.1 libz.so
         cd ../../../../
 fi
 rm ont-guppy_${GUPPY_VER}_linux64.tar.gz
